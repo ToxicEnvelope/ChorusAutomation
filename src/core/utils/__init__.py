afe7658b3720f877a4a71d9db83be978ba16dbda
Yahav N. Hoffman <23465872+ToxicEnvelope@users.noreply.github.com>
2019-12-05 13:55:32 +0200

    Update __init__.py

--- src/core/utils/__init__.py
+++ src/core/utils/__init__.py
@@ -1,10 +1,21 @@
 import os
+from time import time
 
 root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
 class GlobalConfig:
     def get_bin_dir():
-        return os.path.join(root_dir, 'bin')
-
+        return os.path.join(root_dir, r'bin')
+    def get_logs_dir():
+        return os.path.join(root_dir, r'src\runtime')
+    def get_logger_format():
+        return r'%(asctime)s - %(levelname)s - %(message)s'
+    def timestamp():
+        return time().__str__()[:10]
 
 if __name__ == '__main__':
     print('->', GlobalConfig.get_bin_dir())
+    print('->', GlobalConfig.get_logs_dir())
+    print('->', GlobalConfig.get_logs_dir())
+    print('->', GlobalConfig.get_logger_format())
+    print('->', GlobalConfig.timestamp())
